25793a6821d469932315c4b92ccb17615975d36d
raghav bhardwaj <braghav474@gmail.com>
2026-02-20 14:34:34 +0530

    basic crud operations

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 import mongoose, { Schema } from "mongoose";
-import e, { urlencoded } from "express";
+import express, { urlencoded } from "express"
 import dotenv from "dotenv"
-const app = e()
 
 dotenv.config({path:"./.env"})
 
 
- async function connectDB () {
-       try {
-          await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`)
-          console.log('mongo db connected!!!!');
-       } catch (error) {
+const app = express()
+
+app.use(express.json())
+app.use(urlencoded())
+
+
+
+// db coonection
+async function connect () {
+     try {
+       await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`)
+        console.log("db connectd");
+     } catch (error) {
         console.log(error);
-       }
-    }
-    connectDB()
+        
+     }
+}
+connect()
+
 
 const userSchema = new Schema({
     name:String,
@@ -25,44 +34,101 @@ const userSchema = new Schema({
 const User = mongoose.model("User",userSchema)
 
 
-// crud operations = create , read , update , delete
+// create,read,update,delete
 
-app.use(e.json())
-app.use(urlencoded())
+app.post("/create-user",async function createUser (req,res) {
+     try {
+        const {name,email,password} = req.body
+        const user = await User.create({name,email,password})
+        return res.status(400).json({message:"ok",user})
+     } catch (error) {
+        console.log(error);
+     }
+})
 
+app.put("/update-user/:id", async function(req,res){
+    try {
+        const {id} = req.params
+        const {newEmail} = req.body
+        const updatedUser = await User.findByIdAndUpdate(id,{email:newEmail}, {new:true})
+        return res.status(200).json({message:"ok",updatedUser})
+    } catch (error) {
+        console.log(error);
+    }    
 
-app.get("/",(req,res)=> {
-    res.send("hello")
+})
+
+app.get("/user/:id",async function(req,res){
+     try {
+        const {id} = req.params
+       const user = await User.findById(id)
+       return res.status(200).json({message:"ok",user})
+     } catch (error) {
+        console.log(error);
+     }
+
+})
+
+app.get("/users",async function(req,res) {
+     try {
+        const users = await User.find({email:"changed2-email@.com"})
+        return res.status(200).json({message:"ok",users})
+     } catch (error) {
+        console.log(error);
+     }
+    
+})
+
+app.get("/all-users",async function(req,res) {
+     try {
+        const users = await User.find({})
+        return res.status(200).json({message:"ok",users})
+     } catch (error) {
+        console.log(error);
+     }
+})
+
+app.post("/register",async function(req,res){
+    const {name} = req.body
+    const existingUser = await User.findOne({name})
+     return res.status(200).json({message:"ok",existingUser})
+
 })
 
 
-app.post("/create-user",async (req,res)=> {
-   const {name,email,password} = req.body
-   const newUser = await User.create({name,email,password})
-   return res.status(200).json({message:"user created successfully",newUser})
+app.delete("/delete",async function (req,res) {
+    try {
+         const {name} = req.body
+        await User.deleteOne({name})
+        return res.status(200).json({message:"ok"})
+    } catch (error) {
+        console.log(error);
+        
+    }
+    
 })
 
-app.get("/get-all-users",async (req,res)=> {
-    const users = await User.find({})
-    return res.status(200).json({message:"users fetched successfully",users})
-
+app.delete("/delete/:id",async function (req,res) {
+    try {
+         const {id} = req.params
+         await User.findByIdAndDelete(id)
+         return res.status(200).json({message:"ok"})
+    } catch (error) {
+        console.log(error);
+    }
+    
 })
 
-app.put("/update/:id",async (req,res)=>{
-    const {id} = req.params
-    const updatedUser = await User.findByIdAndUpdate(id,{name:"changed"},{new:true})
-   return res.status(200).json({message:`user updated successfully`,updatedUser})
-})
 
 
-app.delete("/delete/:id",async (req,res)=> {
-       const {id} = req.params
-      await User.findByIdAndDelete(id)
-      return res.status(200).json({message:"user deleted successfully"})
-})
+
 
 
 
 app.listen(3333,()=> {
-    console.log("app is running on port 3333");
+     console.log("app is running on port 3333");
 })
+
+
+
+
